c2c00efc68b23f03c52ba5341c5fbaab13309205
Ed Bishop <edjbishop@googlemail.com>
2014-05-06 22:02:16 +0100

    Moved the interval into a function to enable toggling it;

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 $(function () {
   // bung the current version of jQuery in the console
   console.log($().jquery);
-  
+
   //$('#show-search').on('click', function (e) {
-    //e.preventDefault();
-    //$('.full-container').toggleClass('push-menu--open');    
+  //e.preventDefault();
+  //$('.full-container').toggleClass('push-menu--open');    
   //});
-  
+
   var pushContent = $('.push-content'),
       pushMenu = $('.push-menu');
-  
-  $(document).keydown(function(e) {
-    if(e.ctrlKey && (e.which == 77)) {
+
+  $(document).keydown(function (e) {
+    if (e.ctrlKey && (e.which == 77)) {
       e.preventDefault();
       pushContent.toggleClass('open closed');
       pushMenu.toggleClass('closed open');
-      setTimeout(function() {
+      setTimeout(function () {
         if ($(pushMenu).hasClass('open')) {
           $('#track-search').focus();
         }
@@ -24,43 +24,51 @@ $(function () {
     }
     return true;
   });
-  
+
 });
 
 
+var howManyTimesAmICalled = 0;
+function getCorrectCoverWidth() {
+  var viewportWidth = 1295; //$("#test").width();
+  console.log("Viewport width: " + viewportWidth + " (" + howManyTimesAmICalled++ + ")");
 
-function viewportWidthCheck() {
-    var viewportWidth = document.documentElement.clientWidth,
-        coversPerRow = 5;
-    
-    if (viewportWidth <= "500") {
-      coversPerRow = 1;
-    }
-    else if (viewportWidth >= "501" && viewportWidth <= "850") {
-      coversPerRow = 2;
-    }
-    else if (viewportWidth >= "851" && viewportWidth <= "1200") {
-      coversPerRow = 3;
-    } 
-    else {
-      coversPerRow = 4;  
-    }
-    
-    var coverWidth = viewportWidth / coversPerRow;
-    return coverWidth;
-}
+  var coversPerRow;
 
-function setAlbumCoverSize() {
-    for (var i = 0; i < albumCoverClass.length; ++i) {
-        albumCoverClass[i].style.width = viewportWidthCheck() + "px";
-        albumCoverClass[i].style.height = viewportWidthCheck() + "px";
-    }
+  if (viewportWidth <= "500") {
+    coversPerRow = 1;
+  }
+  else if (viewportWidth >= "501" && viewportWidth <= "850") {
+    coversPerRow = 2;
+  }
+  else if (viewportWidth >= "851" && viewportWidth <= "1200") {
+    coversPerRow = 3;
+  }
+  else {
+    coversPerRow = 4;
+  }
+
+  var coverWidth = viewportWidth / coversPerRow;
+  return coverWidth;
 }
 
 var albumCoverClass = document.getElementsByClassName("album-cover");
 
 // Setting a timer and polling for the screen size every 500ms seems smoother
-setInterval(setAlbumCoverSize, 500);
-
-//setAlbumCoverSize();
-//window.onresize = setAlbumCoverSize;
+var albumCoverSizeInterval;
+function toggleResizing() {
+  if (albumCoverSizeInterval) {
+    clearInterval(albumCoverSizeInterval);
+    albumCoverSizeInterval = null;
+  } else {
+    albumCoverSizeInterval = setInterval(setAlbumCoverSize, 500);
+  }
+}
+function setAlbumCoverSize() {
+  var correctCoverWidth = getCorrectCoverWidth();
+  for (var i = 0; i < albumCoverClass.length; ++i) {
+    albumCoverClass[i].style.width = correctCoverWidth + "px";
+    albumCoverClass[i].style.height = correctCoverWidth + "px";
+  }
+}
+toggleResizing();
